e242d6a1d2847fcbdcdfa25942d823d4a420c52c
Kirill Greshnov <k.greshnov@innopolis.university>
2025-02-12 15:55:01 +0300

    Added .gitignore, info.py and main.py; removed test.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+import asyncio
+import logging
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+
+from info import TOKEN
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+@dp.message()
+async def echo_handler(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
+
+async def main():
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
