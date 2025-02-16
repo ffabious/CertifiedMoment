2cd74a00b6a3434d86461af652c104ae439de565
Kirill Greshnov <k.greshnov@innopolis.university>
2025-02-16 13:53:10 +0300

    Changed start msg; added reply keyboard to interact with the bot; added simple menu and settings msgs; replaced echo handler to notify user about unknown msgs

--- main.py
+++ main.py
@@ -5,64 +5,72 @@ import shelve
 
 from aiogram.types import Message, FSInputFile
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from aiogram.filters import CommandStart
 
 from static.file_paths import start_message_image_path, chat_message_db_path
-from static.texts import start_message_gen_
+from static.texts import start_message_gen_, menu_button_text, settings_button_text
+from static.texts import unknown_message_response_text, jojo_reference_text
 from controller import dp, bot
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
-    photo = FSInputFile(path=start_message_image_path, filename="start_image.jpeg")
-    test_button = InlineKeyboardButton(text="💡 Test Button", callback_data="test_button_callback")
-    markup = InlineKeyboardMarkup(inline_keyboard=[[test_button]])
+    menu_button = KeyboardButton(text=menu_button_text)
+    settings_button = KeyboardButton(text=settings_button_text)
+    regular_markup = ReplyKeyboardMarkup(
+        keyboard=[[menu_button, settings_button]],
+        resize_keyboard=True
+    )
+    start_photo = FSInputFile(
+        path=start_message_image_path,
+        filename="start_image.jpeg"
+    )
     prev_message = await message.answer_photo(
+        photo=start_photo,
         caption=start_message_gen_(message.from_user.full_name),
-        photo=photo,
-        reply_markup=markup
-        )
-    with shelve.open(chat_message_db_path) as chat_msg_db:
-        chat_msg_db[str(prev_message.chat.id)] = prev_message.message_id
-    
+        reply_markup=regular_markup
+    )
 
+    with shelve.open(chat_message_db_path) as chat_msg_db:
+        chat_msg_db[str(message.from_user.id)] = prev_message.message_id
+
+
+# User chose to open menu
+@dp.message(lambda msg: msg.text == menu_button_text)
+async def menu_button_handler(message: Message):
+    inline_button = InlineKeyboardButton(
+        text="Basic Menu Button",
+        callback_data="inline_menu_callback"
+    )
+    inline_markup = InlineKeyboardMarkup(inline_keyboard=[[inline_button]])
+    await message.answer(
+        text="This is a basic menu message",
+        reply_markup=inline_markup,
+    )
+
+@dp.message(lambda msg: msg.text == settings_button_text)
+async def settings_button_handler(message: Message):
+    inline_button = InlineKeyboardButton(
+        text="Basic Settings Button",
+        callback_data="settings_inline_callback"
+    )
+    inline_markup = InlineKeyboardMarkup(inline_keyboard=[[inline_button]])
+    await message.answer(
+        text="This is a basic settings message",
+        reply_markup=inline_markup
+    )
+
+# Handler for a reference I wanted to add (this doesn't influence the bot functionality)
+@dp.message(lambda msg: 'ramazan' in msg.text.lower() or 'ramzeus' in msg.text.lower())
+async def jojo_reference_handler(message: Message):
+    await message.answer(text=jojo_reference_text)
+
+# To handle random messages which aren't connected to the functionality of the bot
 @dp.message()
-async def echo_handler(message: Message):
-    await bot.delete_message(
-        chat_id=message.chat.id,
-        message_id=message.message_id
-    )
-    with shelve.open(chat_message_db_path) as chat_msg_db:
-        if str(message.chat.id) in list(chat_msg_db.keys()):
-            await bot.delete_message(
-                chat_id=message.chat.id,
-                message_id=chat_msg_db[str(message.chat.id)]
-            )
-    try:
-        prev_message = await message.send_copy(chat_id=message.chat.id)
-        with shelve.open(chat_message_db_path) as chat_msg_db:
-            chat_msg_db[str(prev_message.chat.id)] = prev_message.message_id
-    except TypeError:
-        prev_message = await message.answer("Nice try!")
-        with shelve.open(chat_message_db_path) as chat_msg_db:
-            chat_msg_db[str(prev_message.chat.id)] = prev_message.message_id
+async def unknown_message_handler(message: Message):
+    await message.answer(text=unknown_message_response_text)
 
-
-@dp.callback_query(lambda c: c.data == "test_button_callback")
-async def test_button_handler(call: CallbackQuery):
-    button = InlineKeyboardButton(
-        text="💸 New button",
-        callback_data="new_button_callback"
-    )
-    markup = InlineKeyboardMarkup(
-        inline_keyboard=[[button]]
-    )
-    await bot.edit_message_caption(
-        chat_id=call.message.chat.id,
-        message_id=call.message.message_id,
-        caption="👀 This message was edited with this exact text!",
-        reply_markup=markup
-    )
 
 async def main():
     await dp.start_polling(bot)
--- static/texts.py
+++ static/texts.py
@@ -1,5 +1,15 @@
 from aiogram import html
 
+dev_tg_username = "didukyril"
+
 def start_message_gen_(user_name: str):
-    end = "Don't forget it!"
-    return f"Hello, {html.bold(user_name)}! 👋\nThis bot was created to help you remember stuff.\n{html.italic(end)}"
+    start_msg = f"""Hey, {html.bold(user_name)}! 👋
+I was created by some chill and (maybe) ADHD guy @{dev_tg_username} to help you (as well as himself) remember stuff.
+{html.italic("Don't forget it!")}
+"""
+    return start_msg
+
+menu_button_text = "📖 Menu"
+settings_button_text = "⚙️ Settings"
+unknown_message_response_text = "😓 Unfortunately, I don't understand regular messages. Please, use buttons below to interact with me!"
+jojo_reference_text = "Oh yeah, this guy named me 😁"
